71b88f028fe5ac7a10cffc17c9cb9d177d5928b9
PhotoWolf <dmslukev@gmail.com>
2021-04-19 22:12:52 +0000

    update

--- _build/jupyter_execute/Introduction.py
+++ _build/jupyter_execute/Introduction.py
@@ -1,29 +1,32 @@
 ## Introduction
 
 Graph Neural Networks (GCNs) are an extension of the familiar Convolutional Neural Network to arbitrary topologies. Given the graph $G=\{V,E\}$, Gilmer et al. (2017) define the message-passing framework of GCNs as
+
 $$x_{i}^{l+1} = \Theta^{l}(v_{i}^{l},\gamma(x_{i}^{l},\{x_{j}^{l}:j\in{}N^{1}_{i}\},e_{ij}))$$
+
 where $N_{i}^{1}$ is the 1-neightborhood of vertex $v_{i}$, and $l$ indexes the $l$-th layer of the model. $x_{i}\in{}R^{n}$ is the feature vector associated with $v_{i}$. We refer to $\gamma{}$ as our aggregation function; typical choices are the sum or max operators, but some more exotic options do exist(ie: LSTMs). $\Theta$ is a neural network of some description, most often a single linear layer [1].
 
-While GCNs are fairly well-studied (they've been a "trending topic" the past few years at ICLR ), we have limited understanding of how well they capture the topological information of $G$. And this is perhaps a glaring omission. The literature finds clear benefit to incorporating structural features into GCNs; in particular: 
-* Pretraining a GCN backbone on various centrality tasks improves the accuracy of downstream classifiers [2]. 
+While GCNs are fairly well-studied, we have a limited understanding of how well they capture the topological information of $G$. The literature finds clear benefit to incorporating structural features into GCNs; in particular:
+
+* Pretraining a GCN on various centrality tasks improves the accuracy of downstream classifiers [2]. 
 * Appending Laplacian eigenvectors to vertex features surpasses existing benchmark peroformance, in some cases quite significantly [3].
 * Retaining centrality information produces more discriminative node embeddings [4]. 
 
-This motivates our desire to assess the structural capacity of GCNs and develop means by which it may be improved. Thus far, [5] is the only work in a similar vein; however, their experimental setting is unclear to the point of being unreproducible. They also include a fixed node ID parameter, which makes it difficult to generalize results outside of a transductive setting. [6] demonstrates the applicability of ML to learning topological graph metrics, but it predates the advent of GCNs.
+This motivates our desire to assess the ability of GCNs to learn graph structure and develop means by which it may be improved. Thus far, [5] is the only work in a similar vein; however, their experimental setting is unclear to the point of being unreproducible. They also include a fixed node ID parameter, which makes it difficult to generalize results outside of a transductive setting. [6] demonstrates the applicability of ML to learning topological graph metrics, but it predates the advent of GCNs.
 
 ## Model Code
 
-It is convient to classify GCNs into two clades: node-wise convolutions and edge-wise convolutions. The former are isotrophic algorithms i.e: $\gamma$ is independent of $x_{i}$ and $x_{j}$ for edge $e_{ij}$. This allows us to generalize to varied topologies without much in the way of computational cost. The prototypical example of a node-wise GCN is the GraphConv architecture [7]:
+It is convient to classify GCNs into two groups: node-wise convolutions and edge-wise convolutions. The former process all edges equivalently i.e: $\gamma$ is independent of $x_{i}$ and $x_{j}$ for $e_{ij}$. This allows us to generalize to varied topologies without much in the way of computational cost. The prototypical example of a node-wise GCN is the GraphConv architecture [7]:
 
 $$x_{i}^{l+1} = \Theta_{1}^{l}(x_{i}^{l+1}) + \Theta_{2}^{l}(\sum_{j\in{}N_{i}^{1}}w_{ij}x_{j}^{l})$$
 
-$w_{ij}$ is the scalar weight assocaited with $e_{ij}$. The model employs two feedforward networks, $\Theta_{1}$ and $\Theta_{2}$, which can project the features of the target node and those aggregated from $N_{i}^{l}$ into different subspaces. Assuming both $\Theta$ are  $R^{kxn}$ matrices, each GraphConv layer is $O(|V|kn + |E|n)$ in time and $O(|V|n + |E|)$ in space.
+for $x_{i}^{l}\in{}R^{n}$ and $x_{i}^{l}\in{}R^{k}$. $w_{ij}$ is the scalar weight assocaited with $e_{ij}$. The model employs two feedforward networks, $\Theta_{1}$ and $\Theta_{2}$, which can project the features of the target node and those aggregated from $N_{i}^{l}$ into different subspaces. Assuming both $\Theta$ are  $R^{kxn}$ matrices, each GraphConv layer is $O(|V|kn + |E|n)$ in time and $O(|V|n + |E|)$ in space.
 
-Isotropy restricts the overall capacity of a GCN, as aggregations must be symmetric in each direction. In many cases, it is actually beneficial to operate on *pairs* of node features, and for that we require an anisotrophic model. Enter edge convolutions, of which the most prominent are the Graph Attention Network (GAT) and it's numerous derivatives. We do not find GATs to be particularily performant (or efficient, for that matter), so we instead choose to focus on EdgeConv [8,9]:
+In many cases, it is actually beneficial to operate on *pairs* of node features, and for that we require edge-wise convolutions, of which the most prominent are the Graph Attention Network (GAT) and it's numerous derivatives. We do not find GATs to be particularily performant (or efficient, for that matter), so we instead choose to focus on EdgeConv [8,9]:
 
 $$x_{i}^{l} = \sum_{j\in{}N_{i}^{1}}w_{ij}\Theta{}^{l}(x_{i}^{l}||x_{j}^{l}-x_{i}^{l})$$
 
-If $\Theta$ is restricted to a $R^{kx2n}$ matrix, EdgeConv possesses a layerwise time complexity of $\textit{O}(|E|kn)$ and a $\textit{O}(|V|n + |E|n)$ space complexity.
+If $\Theta$ is restricted to a $R^{k\times{}2n}$ matrix, EdgeConv possesses a layerwise time complexity of $\textit{O}(|E|kn)$ and a $\textit{O}(|V|n + |E|n)$ space complexity.
 
 Our implementations follow each layer with a 1D BatchNorm and LeakyReLU activation [10,11].
 
@@ -89,17 +92,18 @@ class EdgeConv(torch.nn.Module):
 
 ## Training Code
 
-Graph centrality measures are used to quantify the structural properties of a network. By training GCNs to replicate more and more complex centralities, we hope to gain insight into how well they incorporate topology and what limitations they posses, if any. Depending on our algorithm's performance, there may also be various practical applications. For example, path-based centralities (betweenness, closeness, etc) are broadly $\textit{O}(|V|^{3})$ and, at best, $\textit{O}(|V||E|)$ [12], so an accurate GCN approximation would be of great interest in analyzing larger networks. 
+Graph centrality measures are used to quantify the structural properties of a network. By training GCNs to predict more and more complex centralities, we hope to gain insight into how well they incorporate topology and what limitations they posses, if any. Depending on our algorithm's performance, there may also be various practical applications. For example, path-based centralities (betweenness, closeness, etc) are broadly $\textit{O}(|V|^{3})$ and, at best, $\textit{O}(|V||E|)$ [12], so an accurate GCN approximation may be of interest in analyzing larger networks. 
 
-Following the example of [5], we define our loss as the L1 Norm between the normalized model output, $\vec{x}'$, and the targeted centrality scores, $\vec{y}$. Both quantities are min-max scaled. We have experimented with alternative noramlization schemes, but this one results in the most reliable convergence. 
+Following the example of [5], we define our loss as the L1 Norm between the normalized model output, $\vec{x}'$, and the targeted centrality scores, $\vec{y}$. Both quantities are min-max scaled.  
 $$\vec{x}' = \frac{\vec{x} - min(\vec{x})}{max(\vec{x}) - min(\vec{x})}$$
 
 $$L = ||\vec{x}' - \vec{y}||_{1}$$
 
 We also define the rank displacement. Let $\vec{u}$ and $\vec{s}$ be vectors in $R^{k}$. $f$ is the *argsort* function i.e: the mapping $f(x_{i},\vec{x})\rightarrow{}r$ where $r=|\{x_{j}:x_{j} > x_{i} \forall{} x_{j}\in{}\vec{x}\}|$. $n$ is an arbitary constant. Then the rank displacement is given as follows:
+
 $$r_{disp}(\vec{u},\vec{s})=\frac{1}{(1+f(s_{i},\vec{s}))^{n}k(k-1)}\sum_{i=0}^{k-1}|f(u_{i},\vec{u}) - f(s_{i},\vec{s})|$$
 
-Centrality is often used to compare individual nodes and ascertain some manner of "relevance". To reflect this, we wanted to included a ranking measure---which ended up being the displacement---as an added metric.
+Centrality is often used to compare individual nodes and ascertain some manner of "relevance". To reflect this, we wanted to included a ranking measure as an added metric.
 
 
 # L1 Norm w/ Min-Max normalization
